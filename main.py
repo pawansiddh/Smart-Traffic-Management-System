4038b72b2f7b330e48c4c3ccb9acaa55a6aaccce
pawan siddh <131910770+pawansiddh@users.noreply.github.com>
2024-01-03 16:45:45 +0530

    Create main.py
    //Inputs:
    Starting node
    Destination node
    Traffic conditions on each edge
    5
    //Outputs:
    Shortest path from the starting node to the destination node
    Steps:
     Initialize an array, dist[], to represent the distance from the starting node
    to all other nodes in the graph.  Initialize an array, prev[], to represent the previous node in the shortest
    path to each node.  Set the distance of the starting node to 0 and the distances of all other
    nodes to infinity.  For each node in the graph, calculate the minimum distance from the starting
    node to that node using the Dynamic Programming technique.  Update the dist[] array with the minimum distances.  Update the prev[] array with the previous node in the shortest path to each
    node.  Use the prev[] array to determine the shortest path from the starting node to
    the

---
+++ main.py
@@ -0,0 +1,19 @@
+$$Algorithm 1: Shortest Path Algorithm using Dynamic Programming$$
+$$Pseudocode:$$
+function shortestPath(start, dest, traffic):
+dist = array of distances from start to other nodes
+prev = array of previous nodes in shortest path
+set dist[start] = 0 and all other distances to infinity
+for each node in graph:
+calculate minimum distance using Dynamic Programming technique
+update dist[] with minimum distance
+update prev[] with previous node in shortest path
+// Determine shortest path using prev[] array
+path = []
+current = dest
+while current != start:
+path.insert(0, current)
+6
+current = prev[current]
+path.insert(0, start)
+return path
